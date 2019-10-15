35783beee215c3a91f8fcccf3f649352ba8ec811
imHarish03 <harishhari302@gmail.com>
2019-10-15 23:57:38 +0530

    * Minor change

--- src/main/java/com/lab/springmvc/config/HibernateConfig.java
+++ src/main/java/com/lab/springmvc/config/HibernateConfig.java
@@ -4,9 +4,11 @@ import org.hsqldb.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 
+@Configuration
 public class HibernateConfig {
 	@Autowired
 	private ApplicationContext context;
